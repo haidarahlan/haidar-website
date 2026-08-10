db2c00b6df722caf4cae04025f61176e5708e7bf
haidarahlan <ahlangaffar@gmail.com>
2026-08-10 07:35:19 +0700

    fix: correct username in site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://haidar-ahlan.github.io",
+  site: "https://haidarahlan.github.io",
   base: "/haidar-website",
 });
